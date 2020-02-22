fd9d39232fc67bff63921c61a3f43d62b7d2e50c
Scott Mosher <scott@scottmosherphotography.com>
2020-02-22 15:37:29 -0700

    Started project

---
+++ MegaDesk (Mine)/MegaDesk (Mine)/Pages/DeskQuote.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MegaDesk__Mine_.Pages
+{
+    public class DeskQuote
+    {
+        // Order Details
+        public int ID { get; set; }
+        public string clientFirstName { get; set; }
+        public string clientLastName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime submitDate { get; set; }
+        public double quoteTotal { get; set; }
+
+        // Desk Information that is in the order
+        public double deskWidth { get; set; }
+        public double deskDepth { get; set; }
+        public int numDrawers { get; set; }
+        public string surfaceMaterial { get; set; }
+        public int rushOption { get; set; }
+    }
+}
